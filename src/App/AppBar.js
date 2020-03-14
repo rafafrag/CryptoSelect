cc0517ac9d9c271ef97bb311bb67287c714c683b
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-03-14 20:54:41 +0000

    Styled AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,42 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
 
 const Bar = styled.div`
   display: grid;
+  margin-bottom: 40px;
   grid-template-columns: 180px auto 100px 100px;
 `;
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`;
+
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({ name, active }) {
+  return (
+    <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+  );
+}
 
 export default function() {
   return (
     <Bar>
-      hello
-      <div> Bit </div>
-      <div> DashBoard </div>
-      <div> Settings </div>
+      <Logo> Bit </Logo>
+      <div />
+      <ControlButton active name="dashboard" />
+      <ControlButton name="setting" />
     </Bar>
   );
 }
