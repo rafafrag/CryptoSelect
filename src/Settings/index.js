598765a4e3ed9f7dacb1167923e6777df133fa2d
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-03-15 17:08:32 +0000

    Add CoinGrip, mapping coinList

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
 import Page from "../Shared/Page";
+import CoinGrip from "./CoinGrid";
 
 export default function() {
   return (
     <Page name="settings">
       <WelcomeMessage />
       <ConfirmButton />
+      <CoinGrip />
     </Page>
   );
 }
