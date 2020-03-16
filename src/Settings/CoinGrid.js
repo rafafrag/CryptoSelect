5c2819bc37c0992854ba6cfc5ac5cb9f59cddf06
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-03-16 20:46:52 +0000

    Responsive Grid auto-fill

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -6,11 +6,9 @@ import CoinTile from "./CoinTile";
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
   grid-gap: 15px;
   margin-top: 40px;
-  /* grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
-  */
 `;
 
 function getCoinsToDisplay(coinList, topSection, favorites) {
