bd916b3a7c4313234ea56651ff5152ef93acddfc
rafafrag <31895680+rafafrag@users.noreply.github.com>
2020-03-16 21:35:12 +0000

    add search favorites input and styling

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
 import Page from "../Shared/Page";
-import CoinGrip from "./CoinGrid";
 import CoinGrid from "./CoinGrid";
+import Search from "./Search";
 
 export default function() {
   return (
     <Page name="settings">
-      <CoinGrid topSection />
       <WelcomeMessage />
+      <CoinGrid topSection />
       <ConfirmButton />
-      <CoinGrip />
+      <Search />
+      <CoinGrid />
     </Page>
   );
 }
